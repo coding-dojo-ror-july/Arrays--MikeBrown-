4c9b3e8cbeb44dd2f2541bd5c3b121de08ce0bd4
Mike Brown <mikebrownn@hotmail.com>
2017-07-03 20:14:06 +0000

    commit

---
+++ arrays.rb
@@ -0,0 +1,24 @@
+arr1 = ["My", "Name", "Is", "Mike", "Brown"]
+arr2 = [1,2,3,4,5,6,7,8,9,10]
+
+#1. puts arr1[0] --will print out the first element in the array
+
+#2. arr1.delete("Brown") --deletes that specific element in the array
+#puts arr1 ---- prints the array after the deletion 
+
+#3. arr1.reverse! --reverses all of the elements in the array
+#puts arr1 ---- prints the array after reversing it 
+
+#4. puts arr1.length -- prints out the length of arr1
+
+#5. puts arr2.sort -- sorts the numbers inside of the array 
+
+#6. puts arr1.slice(0..4) -- Grabs all of the elements from 0 to 4th index
+
+#7. puts arr1.shuffle -- Shuffles everything in the array 
+
+#8. puts arr1.insert(5,"Hello") -- inserts "Hello" into the 5th index.
+
+#9. puts arr1.values_at(0,1,2) -- prints out whatever values are at the specified index
+
+
